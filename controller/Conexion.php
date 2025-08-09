76be6bb726864f85efd4de2d8a814d4d69524d3b
DIEGO CASTAÑEDA <dcc023@inlumine.ual.es>
2025-08-09 18:23:08 +0200

    Creando rama Diego

--- controller/Conexion.php
+++ controller/Conexion.php
@@ -2,7 +2,7 @@
 
 include_once('DatosConexion.php');
 
-$mysqli = mysqli_connect($host, $user, $password, $database, port: 3308);
+$mysqli = mysqli_connect($host, $user, $password, $database, port: 3306);
 
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error . "<br/>";
