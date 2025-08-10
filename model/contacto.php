8a5b213702042bfaaf4a8d895c11236c6cddfdd7
lomina <pro.sketch156@slmail.me>
2025-08-10 18:44:37 +0200

    visualizacion web dockerizada

--- model/contacto.php
+++ model/contacto.php
@@ -10,46 +10,46 @@ $telefono = "";
 $correo = "";
 
 
-if($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-    $contacto = new Contacto();
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        $contacto = new Contacto();
 
-    if($_SESSION['admin']=='1') { //ADMIN PAGINA DE MODIFICACIONES
+        if($_SESSION['admin']=='1') { //ADMIN PAGINA DE MODIFICACIONES
 
-        $modExitosa = $contacto->validar($_POST);
+            $modExitosa = $contacto->validar($_POST);
 
-        if($modExitosa !="")
-        {
-            echo "<div style='text-align:center;font-size:12px;color:white; background-color:gey;'>";
-            echo $modExitosa;
-            echo "</div>";
-        }else
-        {
-            header('Location: modExitosa.html');
-            die;
+            if($modExitosa !="")
+            {
+                echo "<div style='text-align:center;font-size:12px;color:white; background-color:gey;'>";
+                echo $modExitosa;
+                echo "</div>";
+            }else
+            {
+                header('Location: modExitosa.html');
+                die;
+            }
+
+        }else{ //FORMULARIO CONTACTO
+
+            $mensajeEnviado = $contacto->contactar($_POST);
+
+            if($mensajeEnviado !="")
+            {
+                echo "<div style='text-align:center;font-size:12px;color:white; background-color:gey;'>";
+                echo $mensajeEnviado;
+                echo "</div>";
+            }else
+            {
+                header('Location: mensajeExitoso.html');
+                die;
+            }
         }
 
-    }else{ //FORMULARIO CONTACTO
-
-        $mensajeEnviado = $contacto->contactar($_POST);
-
-        if($mensajeEnviado !="")
-        {
-            echo "<div style='text-align:center;font-size:12px;color:white; background-color:gey;'>";
-            echo $mensajeEnviado;
-            echo "</div>";
-        }else
-        {
-            header('Location: mensajeExitoso.html');
-            die;
-        }
+        $mensaje = $_POST['mensaje'];
+        $nombre = $_POST['nombre'];
+        $apellidos = $_POST['apellidos'];
+        $telefono = $_POST['telefono'];
+        $correo = $_POST['email'];
     }
-
-    $mensaje = $_POST['mensaje'];
-    $nombre = $_POST['nombre'];
-    $apellidos = $_POST['apellidos'];
-    $telefono = $_POST['telefono'];
-    $correo = $_POST['email'];
-}
 
 ?>
